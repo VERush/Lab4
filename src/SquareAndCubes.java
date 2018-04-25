dcfb966d02e2e4d9d3b8c0356e4a64a67a4e66f3
VERush <Vicki@rushworld.com>
2018-04-25 16:32:08 -0400

    Printf formatting & calculations completed

--- src/SquareAndCubes.java
+++ src/SquareAndCubes.java
@@ -9,13 +9,23 @@ public class SquareAndCubes {
 		int num;
 		String cont = "y";
 		System.out.println("Learn your squares and cubes!");
-		System.out.println();
 
-		System.out.print("Enter an integer: ");
-		num = scan.nextInt();
-		System.out.println();
-		System.out.printf("Number/tSquared/tCubed");
+		while (cont.equalsIgnoreCase("y")) {
+			System.out.println();
+			System.out.print("Enter an integer: ");
+			num = scan.nextInt();
+			System.out.println();
+			System.out.printf("%1$-11s %2$-11s %3$-11s%n", "Number", "Squared", "Cubed");
+			System.out.printf("%1$-11s %2$-11s %3$-11s%n", "=======", "=======", "======");
 
+			for (int i = 1; i <= num; i++) {
+				System.out.printf("%1$-11s %2$-11s %3$-11s%n", i, i * i, i * i * i);
+			}
+			System.out.printf("%nContinue? (y/n): ");
+			cont = scan.next();
+		}
+		System.out.println();
+		System.out.println("Bye!");
 		scan.close();
 	}
 
